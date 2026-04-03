b8c965d18573a83af069d7994c0a04cc78571daa
Helweras <helweras@gmail.com>
2026-04-03 19:24:25 +0200

    Первичная логика эксперимента кровные узы

--- Experiments/PlaygroundParadox/Logic.py
+++ Experiments/PlaygroundParadox/Logic.py
@@ -11,4 +11,37 @@ def gen_random_weight():
     return weights
 
 
-child_house = ChildHouse()
+def family_connect(child_list: list):
+    check = [child_list[0].second_name]
+    for child in child_list[1:]:
+        if child.second_name in check:
+            return True
+        else:
+            check.append(child.second_name)
+    return False
+
+
+def get_class(all_ch, value):
+    result = random.sample(all_ch, value)
+    for i in result:
+        print(i.second_name, end=" ")
+    print()
+    return result
+
+
+def blood_tiles(count_sim, value):
+    c = 0
+    weight = gen_random_weight()
+    child_house = ChildHouse(weights_born=(55, 33, 9, 3, 1))
+    all_children = child_house.all_children_list
+    for _ in range(count_sim):
+        result = random.sample(all_children, value)
+        if family_connect(result):
+            c += 1
+        print(f"\r{c}", end="")
+    print()
+    return round(c / count_sim, 2) * 100
+
+
+x = blood_tiles(100000, 20)
+print(x)
