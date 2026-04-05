52eac842b5fea2b1ed4863d7129845da20238b78
Helweras <helweras@gmail.com>
2026-04-05 21:59:14 +0200

    Казино в терминале

---
+++ Experiments/PlaygroundParadox/Components/casino_terminal.py
@@ -0,0 +1,54 @@
+import random
+
+
+def gen_random_weight():
+    points = sorted([random.randrange(101) for _ in range(4)])
+    points = [0] + points + [100]
+
+    weights = tuple(next_val - current for current, next_val in zip(points, points[1:]))
+
+    return weights
+
+
+def get_random_value(start=7, stop=22):
+    return random.randrange(start, stop)
+
+
+def win(real, prog, stav):
+    if abs(real - prog) <= 2:
+        return stav * 5
+    elif 2 < abs(real - prog) <= 10:
+        return stav * 2
+    elif 10 < abs(real-prog) <= 20:
+        return stav
+    else:
+        return 0
+
+
+
+def casino(coin, blood_tiles_fun):
+    dk = coin
+
+    while dk > 0:
+        post_request = {
+            "weight": gen_random_weight(),
+            "value": get_random_value(),
+            "count_sim": 1000,
+            "num_of_family": 100
+        }
+        print(f"dk = {dk}")
+        print("распределение")
+        print(post_request["weight"])
+        print("размер группы = ", post_request["value"])
+        stavka = int(input("ваша ставка - "))
+        while stavka > dk:
+            stavka = int(input("ваша ставка - "))
+        dk -= stavka
+        your_num = int(input("ваш прогноз - "))
+        real = blood_tiles_fun(post_request)["result"]
+        print(f"real = {real}\nyour = {your_num}")
+        dk += win(real=real, prog=your_num, stav=stavka)
+        print(f"your dk = {dk}")
+        input("Нажми Enter что бы продолжить")
+        print("=================================")
+    print("end")
