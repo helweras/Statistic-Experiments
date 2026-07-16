9cb2362f27be77340205166e4997ed9ae0bc7d46
admin <helweras@gmail.com>
2026-07-16 16:37:49 +0200

    Добавлена функция валидации данных и соханения данных в state

--- frontend/src/state/monty_hall_state.py
+++ frontend/src/state/monty_hall_state.py
@@ -1,10 +1,30 @@
 import streamlit as st
+import pandas as pd
 
 
-def init_monty_hall_state():
-    """Резервирует места под ответы бэкенда в памяти сессии."""
-    if "single_result" not in st.session_state:
-        st.session_state.single_result = None
+class MontyHallState:
 
-    if "explore_result" not in st.session_state:
-        st.session_state.explore_result = None
+    def __init__(self):
+        """Резервирует места под ответы бэкенда в памяти сессии."""
+        if "single_result" not in st.session_state:
+            st.session_state.single_result = None
+
+        if "explore_result" not in st.session_state:
+            st.session_state.explore_result = None
+
+    @property
+    def explore_df(self) -> pd.DataFrame:
+        """Возвращает DataFrame исследования, если он есть, иначе пустой."""
+        if st.session_state.explore_result is None:
+            return pd.DataFrame()
+        return pd.DataFrame(st.session_state.explore_result)
+
+    @staticmethod
+    def set_explore_result(data):
+        """Записывает ответ от бэкенда для исследования."""
+        st.session_state.explore_result = data
+
+    @staticmethod
+    def set_single_result(data):
+        st.session_state.single_result = data
+
--- frontend/src/views/monty_hall.py
+++ frontend/src/views/monty_hall.py
@@ -1,16 +1,44 @@
 import streamlit as st
-from typing import Dict, Optional, Any
 from src.components.render_description import render_description_experiment
+from src.state.monty_hall_state import MontyHallState
 from src.api.api_monty_hall import post_simulate
 from config import MontyHallEndpoints
 from enum import Enum
+from typing import Dict, Union
+
+def _render_explore(state: MontyHallState):
+    with st.container(border=True):
+        st.write("Выясним, как меняется преимущество смены выбора с увеличением общего числа закрытых дверей.")
+        # 1. Блок ввода параметров
+        c1, c2, c3 = st.columns(3)
+        range_close_doors = st.slider("Диапазон закрытых количество дверей", 6, 20, (6, 10))
+        min_close_doors, max_close_doors = range_close_doors
+        iterable = c3.select_slider("Итераций", options=[50, 75, 100, 150], value=75)
+        count_doors = c2.number_input("Количество дверей", max_close_doors + 2, 22, max_close_doors + 2)
+        count_prize = c1.number_input("Количество призов", 1, min_close_doors, 1)
+        st.markdown(f"### 🚀 Симуляция стартует с **{min_close_doors}** и до **{max_close_doors}** закрытых дверей.")
+        if st.button("Запустить исследование", type="primary"):
+            data_batch = {"simulations": []}
+            start_range = int(min_close_doors) if min_close_doors is not None else 1
+            end_range = int(max_close_doors) if max_close_doors is not None else 10
+            for close_doors in range(start_range, end_range + 1):
+                data = {
+                    "count_prize": count_prize,
+                    "count_doors": count_doors,
+                    "closed_doors": close_doors,
+                    "iterable": iterable
+                }
+                data_batch["simulations"].append(data)
+            simulate = MontyHallEndpoints.SIMULATE.value
+            response = post_simulate(data_batch, simulate)
+            state.set_explore_result(response)
+
 
 
 class MontyHallExploreType(str, Enum):
     CLOSED_DOORS = "closed_doors"
-    DOORS = "doors"
-    PRIZES = "prizes"
-
+    DOORS = "count_doors"
+    PRIZES = "count_prizes"
 
 
 MONTY_HALL_EXPLORE_CONFIGS = {
@@ -52,48 +80,61 @@ MONTY_HALL_EXPLORE_CONFIGS = {
 }
 
 
-def _render_explore():
-    with st.container(border=True):
-        st.write("Выясним, как меняется преимущество смены выбора с увеличением общего числа закрытых дверей.")
-        # 1. Блок ввода параметров
-        c1, c2, c3 = st.columns(3)
-        range_close_doors = st.slider("Диапазон закрытых количество дверей", 6, 20, (6,10))
-        min_close_doors, max_close_doors = range_close_doors
-        iterable = c3.select_slider("Итераций", options=[50, 75, 100, 150], value=75)
-        count_doors = c2.number_input("Количество дверей", max_close_doors + 2, 22, max_close_doors + 2)
-        count_prize = c1.number_input("Количество призов", 1, min_close_doors, 1)
-        st.markdown(f"### 🚀 Симуляция стартует с **{min_close_doors}** и до **{max_close_doors}** закрытых дверей.")
-        if st.button("Запустить исследование", type="primary"):
-            data_batch = {"simulations": []}
-            start_range = int(min_close_doors) if min_close_doors is not None else 1
-            end_range = int(max_close_doors) if max_close_doors is not None else 10
-            for close_doors in range(start_range, end_range + 1):
-                data = {
-                    "count_prize": count_prize,
-                    "count_doors": count_doors,
-                    "closed_doors": close_doors,
-                    "iterable": iterable
-                }
-                data_batch["simulations"].append(data)
-            simulate = MontyHallEndpoints.SIMULATE.value
-            response = post_simulate(data_batch, simulate)
-            print(response)
+
+def valid_data(data: Dict[str, list[Dict[str, Union[int, float, None]]]], index):
+    test_data_batch = data["simulations"]
+
+    for batch in test_data_batch:
+        count_prize = batch.get("count_prize")
+        closed_doors = batch.get("closed_doors")
+        count_doors = batch.get("count_doors")
+
+        # 1. Валидация на заполненность
+        if count_prize is None or count_doors is None:
+            st.error(f"❌ Ошибка в симуляции №{index}: Пожалуйста, заполните все поля числовыми значениями!")
+            st.warning("Проблемные данные:")
+            st.json(batch)  # Выводим конкретный неправильный кусочек данных
+            return False
+
+        # 2. Валидация логики (призы < дверей)
+        if count_prize >= count_doors:
+            st.error(f"❌ Ошибка в симуляции №{index}: Количество призов должно быть меньше количества дверей!")
+            st.warning("Проблемные данные:")
+            st.json(batch)
+            return False
+
+        # 3. Математическая валидация парадокса
+        if (1 + closed_doors) >= count_doors:
+            st.error(
+                f"❌ Ошибка в симуляции №{index}: Ведущий не может оставить закрытыми {closed_doors} дв. при общем количестве {count_doors}!")
+            st.warning("Проблемные данные:")
+            st.json(batch)
+            return False
+
+        # 4. Валидация призов и закрытых дверей
+        if count_prize > closed_doors:
+            st.error(
+                f"❌ Ошибка в симуляции №{index}: Количество призов должно быть меньше или равно количеству закрытых дверей!")
+            st.warning("Проблемные данные:")
+            st.json(batch)
+            return False
+
+    return True
 
 
-def _render_explore_generic(explore_type: MontyHallExploreType):
+def _render_explore_generic(explore_type: MontyHallExploreType, state: MontyHallState):
     cfg = MONTY_HALL_EXPLORE_CONFIGS[explore_type]
 
     with st.container(border=True):
         st.write(cfg["description"])
-
 
         # Разметка на 3 колонки
         c1, c2, c3 = st.columns(3)
 
         # 1. Главный слайдер исследования (Динамическая переменная)
         # Он всегда определяет верхнюю границу диапазона (max_val)
-        min_val, max_val= st.slider(cfg["slider_label"], cfg["slider_min"], cfg["slider_max"], cfg["slider_default"], key=f"slider_{explore_type.value}")
-
+        min_val, max_val = st.slider(cfg["slider_label"], cfg["slider_min"], cfg["slider_max"], cfg["slider_default"],
+                                     key=f"slider_{explore_type.value}")
 
         # Колонки итераций (она всегда одинаковая во всех тестах)
         iterable = c3.select_slider("Итераций", options=[50, 75, 100, 150], value=75, key=f"it_{explore_type.value}")
@@ -101,16 +142,24 @@ def _render_explore_generic(explore_type: MontyHallExploreType):
         # 2. Динамическая отрисовка ФИКСИРОВАННЫХ параметров в c1 и c2
         # Мы не знаем заранее, что там будет, пока не заглянем в cfg
         fix_val_1 = int(
-            c1.number_input(cfg["c1_label"], min_value=cfg["c1_min"], value=cfg["c1_default"], step=1, key=f"fix1_{explore_type.value}"))
+            c1.number_input(cfg["c1_label"], min_value=cfg["c1_min"], value=cfg["c1_default"], step=1,
+                            key=f"fix1_{explore_type.value}"))
         fix_val_2 = int(
-            c2.number_input(cfg["c2_label"], min_value=cfg["c2_min"], value=cfg["c2_default"], step=1, key=f"fix2_{explore_type.value}"))
+            c2.number_input(cfg["c2_label"], min_value=cfg["c2_min"], value=cfg["c2_default"], step=1,
+                            key=f"fix2_{explore_type.value}"))
 
-        st.markdown(f"### 🚀 Симуляция запускается в диапазоне от **{min_val}** до **{max_val}**.")
+        st.markdown(f"### 🚀 Симуляция запускается в диапазоне от **{min_val}** до **{max_val}**."
+                    f"Всего будет {max_val-min_val+1} точек на графике")
 
         if st.button("Запустить исследование", type="primary", key=f"btn_{explore_type.value}"):
+            if max_val - min_val + 1 < 5:
+                st.error("Количество точек на графике должно быть 5 или больше")
+                return
             data_batch = {"simulations": []}
+            index = 0
 
             for current_step in range(min_val, max_val + 1):
+                index += 1
                 # Создаем пустую заготовку под итерацию
                 sim_data = {"iterable": int(iterable)}
 
@@ -125,35 +174,34 @@ def _render_explore_generic(explore_type: MontyHallExploreType):
 
                 # Приводим ключи словаря к именам, которые строго ожидает Pydantic на бэкенде:
                 # На бэкенде поля называются count_prize и count_doors, а в Enum у нас prizes и total_doors
-                final_payload = {
-                    "count_prize": sim_data.get("prizes", fix_val_1 if cfg["c1_param"] == "prizes" else fix_val_2),
-                    "count_doors": sim_data.get("total_doors",
-                                                fix_val_1 if cfg["c1_param"] == "total_doors" else fix_val_2),
-                    "closed_doors": sim_data.get("closed_doors",
-                                                 fix_val_1 if cfg["c1_param"] == "closed_doors" else fix_val_2),
-                    "iterable": int(iterable)
+                payload = {
+                    cfg["c1_param"]: fix_val_1,
+                    cfg["c2_param"]: fix_val_2,
+                    explore_type.value: current_step,  # Динамический ключ пишется прямо здесь!
+                    "iterable": iterable
                 }
 
-                # Корректируем значение динамического шага, чтобы оно точно перезаписало payload
-                if explore_type == MontyHallExploreType.PRIZES:
-                    final_payload["count_prize"] = current_step
-                elif explore_type == MontyHallExploreType.DOORS:
-                    final_payload["count_doors"] = current_step
-                elif explore_type == MontyHallExploreType.CLOSED_DOORS:
-                    final_payload["closed_doors"] = current_step
 
-                data_batch["simulations"].append(final_payload)
+                data_for_test = {"simulations": [payload]}
+                if valid_data(data_for_test, index=index):
+
+                    data_batch["simulations"].append(payload)
+                else:
+                    return
 
             # Отправка на бэкенд FastAPI
+            print(data_batch)
             simulate_endpoint = MontyHallEndpoints.SIMULATE.value
             with st.spinner("Загрузка данных с бэкенда..."):
                 response = post_simulate(data_batch, simulate_endpoint)
 
             st.success("🎉 Исследование успешно завершено!")
-            st.json(response)
+            state.set_explore_result(response)
+            st.text(state.explore_df)
+            st.rerun()
 
 
-def _render_input_form() -> Optional[Dict[str, Any]]:
+def _render_input_form(state: MontyHallState):
     """
     Отрисовывает форму Streamlit для сбора параметров эксперимента.
     """
@@ -183,37 +231,23 @@ def _render_input_form() -> Optional[Dict[str, Any]]:
         submitted = st.form_submit_button("Симуляция")
 
         if submitted:
-            if count_prize is None or count_doors is None:
-                st.error("Пожалуйста, заполните все поля числовыми значениями!")
-                return None
-
-            # Валидация логики
-            if count_prize >= count_doors:
-                st.error("Количество призов должно быть меньше количества дверей!")
-                return None
-
-            # Математическая валидация парадокса
-            if (1 + closed_doors) >= count_doors:
-                st.error(f"Ведущий не может оставить закрытыми {closed_doors} дв. при общем количестве {count_doors}!")
-                return None
-
             data = {"simulations":
                 [
                     {
-                "count_prize": count_prize,
-                "count_doors": count_doors,
-                "closed_doors": closed_doors,
-                "iterable": iterable
-            }]}
-            simulate = MontyHallEndpoints.SIMULATE.value
-            response = post_simulate(data, simulate)
-            print(response)
+                        "count_prize": count_prize,
+                        "count_doors": count_doors,
+                        "closed_doors": closed_doors,
+                        "iterable": iterable
+                    }]}
 
-    return None
+            if valid_data(data, 1):
+                simulate = MontyHallEndpoints.SIMULATE.value
+                response = post_simulate(data, simulate)
+                state.set_single_result(response)
 
 
 def render():
+    state = MontyHallState()
     render_description_experiment("Monty_Hall")
-    _render_input_form()
-    _render_explore()
-    _render_explore_generic(MontyHallExploreType.PRIZES)
+    _render_input_form(state=state)
+    _render_explore_generic(MontyHallExploreType.PRIZES, state)
